c3f101675182515f1154076f72b844c5c04d7afd
Rostislav <rostkar@gmail.com>
2022-11-27 06:39:56 +0300

    IDE test

--- Welcome.java
+++ Welcome.java
@@ -11,6 +11,7 @@ public class Welcome
       System.out.println(greeting);
       for (int i = 0; i < greeting.length(); i++)
          System.out.print("=");
-      System.out.println();
+            System.out.println();
+      System.out.println("Another line");
    }
 }
